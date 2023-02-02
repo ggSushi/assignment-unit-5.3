3220a28d0a44c43179fb0700e5cd6ca261ded433
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-01 22:38:10 -0600

    got the first function working. now testing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,4 +1,25 @@
 console.log('***** Music Collection *****')
+// Creating variable called `collection` as an empty array
+let collection = [];
+// adding function `addToCollection`
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        songTitle: title,
+        songArtist: artist,
+        songYearPublished: yearPublished,
+    }
+    collection.push(album);
+    return `Added ${album.songTitle}`;
+}
+// testing and adding to collection
+console.table(addToCollection(`"Boys Need Love"`, `David So feat. Timothy Delaghetto`, `2019`));
+addToCollection(`"Am I Wrong"`, `Anderson .Paak feat. ScHoolboy Q`, `2016`);
+addToCollection(`""`, ``, ``);
+addToCollection(`""`, ``, ``);
+addToCollection(`""`, ``, ``);
+addToCollection(`""`, ``, ``);
+
+
 
 
 
---
+++ assignment/scripts/stretchGoals.js
@@ -0,0 +1,1 @@
+console.log(`Stretch Goals!`);
