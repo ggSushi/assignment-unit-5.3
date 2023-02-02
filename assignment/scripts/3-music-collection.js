66cb55f927b65da9388753733177e88bba1a6bb3
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-01 23:45:55 -0600

    added more songs to collection and changed the test logs

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,12 +12,15 @@ function addToCollection(title, artist, yearPublished) {
     return album;
 }
 // testing and adding to collection
-console.log('Added', addToCollection(`"Boys Need Love"`, `David So feat. Timothy Delaghetto`, `2019`));
-console.log('Added', addToCollection(`"Am I Wrong"`, `Anderson .Paak feat. ScHoolboy Q`, `2016`));
+console.log('Added', addToCollection(`"Boys Need Love"`, `David So`, `2019`));
+console.log('Added', addToCollection(`"Am I Wrong"`, `Anderson .Paak`, `2016`));
 console.log('Added', addToCollection(`"Run Wild"`, `Jon Bellion`, `2014`));
 console.log('Added', addToCollection(`"HUMBLE"`, `Kendrick Lamar`, `2017`));
 console.log('Added', addToCollection(`"Fine"`, `Kyle Hume`, `2022`));
 console.log('Added', addToCollection(`"Blood Red Summer"`, `Coheed and Cambria`, `2003`));
+//adding extra for testing
+console.log('Added', addToCollection(`"sun and moon"`, `Anees`, `2022`));
+console.log('Added', addToCollection(`"All Time Low"`, `Jon Bellion`, `2017`));
 
 // logging the `collection`
 //? Preferred to use the .table
@@ -40,9 +43,12 @@ function findByArtist(artist) {
     for (let i=0; i < collection.length; i++){
         if (artist === collection[i].songArtist) {
             filterArtist.push(collection[i]);
-            
         }
     }
-    console.log(filterArtist);
+  return filterArtist;
 }
-findByArtist(`Kyle Hume`);
+
+// testing findByArtist function
+console.log(findByArtist(`Kendrick Lamar`)); // should display the song "HUMBLE" info
+console.log(findByArtist(`Jon Bellion`)); // should display 2 tracks by Jon Bellion
+console.log(findByArtist(`PSY`)); // should display empty array
