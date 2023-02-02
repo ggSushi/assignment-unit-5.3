aa87c3c895ef13e75a72ce5f916e349f355010be
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-02 02:15:36 -0600

    finished first stretch with JSON.stringify

--- assignment/scripts/stretchGoals.js
+++ assignment/scripts/stretchGoals.js
@@ -1,11 +1,17 @@
 console.log(`Stretch Goals!`);
 
-//creating a search function
+// creating a search function
 function search(object){
     let searchResult = [];
-//! Originally, (object === undefined) was set below final if statement
-//! This gave an error in which 
-    if (object === undefined) {
+//! Originally, (object === undefined) was set below final if statement.
+//! This gave an error in which it was unable to read `artist` for 
+//! undefined object. Moving it up to be the first thing to resolve allowed
+//! it to pass without needing to read the keys of the object.
+    if (object === undefined || JSON.stringify(object) === `{}`) {
+        //! JSON.stringify(object) === `{}` turns the object put inside the () into a string.
+        //! so, in this case, if the string equals `{}`, which it does, it will return collection.
+        //! This passes the empty object test because it's cheeky and I
+        //! couldn't think of or find other solutions. Don't be mad at me, Chris lol
         return collection;
     } else {
         for (let track of collection) {
@@ -16,11 +22,16 @@ function search(object){
     }
     if (searchResult.length >= 0) {
         return searchResult;
-    } 
+    } else {
+        return collection;
+    }
 }
-// testing le function
-console.log(search({artist: `Jon Bellion`, year: `2017` }));
-console.log(search({artist: `Jon Bellion`, year: `2018` }));
-console.log(search());
-//testing empty object
-console.log(search({}));
+//* testing le function
+console.log(search({artist: `Jon Bellion`, year: `2017` })); // should return searchResult
+console.log(search({artist: `Jon Bellion`, year: `2018` })); // should return empty array
+//* testing undefined object
+console.log(search()); // should return collection
+//* testing empty object
+console.log(search({})); //? should return collection
+
+
