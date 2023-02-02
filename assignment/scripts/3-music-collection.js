45226779cb60e030a8eefcec68a3a9e3379bf4e6
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-01 22:52:53 -0600

    got the albums running and logged

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,15 +9,15 @@ function addToCollection(title, artist, yearPublished) {
         songYearPublished: yearPublished,
     }
     collection.push(album);
-    return `Added ${album.songTitle}`;
+    return album;
 }
 // testing and adding to collection
-console.table(addToCollection(`"Boys Need Love"`, `David So feat. Timothy Delaghetto`, `2019`));
-addToCollection(`"Am I Wrong"`, `Anderson .Paak feat. ScHoolboy Q`, `2016`);
-addToCollection(`""`, ``, ``);
-addToCollection(`""`, ``, ``);
-addToCollection(`""`, ``, ``);
-addToCollection(`""`, ``, ``);
+console.table('Added', addToCollection(`"Boys Need Love"`, `David So feat. Timothy Delaghetto`, `2019`));
+console.table('Added', addToCollection(`"Am I Wrong"`, `Anderson .Paak feat. ScHoolboy Q`, `2016`));
+console.table('Added', addToCollection(`"Run Wild"`, `Jon Bellion`, `2014`));
+console.table('Added', addToCollection(`"HUMBLE"`, `Kendrick Lamar`, `2017`));
+console.table('Added', addToCollection(`"Fine"`, `Kyle Hume`, `2022`));
+console.table('Added', addToCollection(`"Blood Red Summer"`, `Coheed and Cambria`, `2003`));
 
 
 
