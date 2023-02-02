eb7798e41fd6ff70533a2f058d9420a13036f95d
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-02 01:11:27 -0600

    stretch 1 almost done

--- assignment/scripts/stretchGoals.js
+++ assignment/scripts/stretchGoals.js
@@ -1,1 +1,26 @@
 console.log(`Stretch Goals!`);
+
+//creating a search function
+function search(object){
+    let searchResult = [];
+//! Originally, (object === undefined) was set below final if statement
+//! This gave an error in which 
+    if (object === undefined) {
+        return collection;
+    } else {
+        for (let track of collection) {
+            if (track.songArtist === object.artist && track.songYearPublished === object.year) {
+                searchResult.push(track);
+            } 
+        }
+    }
+    if (searchResult.length >= 0) {
+        return searchResult;
+    } 
+}
+// testing le function
+console.log(search({artist: `Jon Bellion`, year: `2017` }));
+console.log(search({artist: `Jon Bellion`, year: `2018` }));
+console.log(search());
+//testing empty object
+console.log(search({}));
